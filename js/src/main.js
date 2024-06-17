ef458ef9fb86228ed08e000de71103238d91db98
alex <aabinaei@gmail.com>
2024-06-16 18:33:11 -0700

    wallet-adapter

---
+++ js/src/main.js
@@ -0,0 +1,46 @@
+import React, { useMemo } from 'react';
+import ReactDOM from 'react-dom/client';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  WalletModalProvider,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export const Wallet = () => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Mainnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          { /* Your app's components go here, nested within the context providers. */}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+function renderr() {
+  const container = document.getElementById('plsss');
+  const root = ReactDOM.createRoot(container);
+  root.render(<Wallet />);
+}
+
+window.PlsRender = renderr;
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
