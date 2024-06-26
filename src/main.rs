7f06567f533f4e48cc4f4b894c615c4c176b9922
alex <aabinaei@gmail.com>
2024-06-26 14:28:06 -0700

    two way channel

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx!(
+    rsx! {
         Nav {}
-        React {}
-    )
+        TwoWay {}
+    }
 }
 
 #[component]
@@ -23,36 +23,63 @@ fn Nav() -> Element {
 }
 
 #[component]
-fn React() -> Element {
-    // Create a future that will resolve once the javascript has been successfully executed.
-    let future = use_resource(move || async move {
-        // The `eval` is available in the prelude - and simply takes a block of JS.
-        // Dioxus' eval is interesting since it allows sending messages to and from the JS code using the `await dioxus.recv()`
-        // builtin function. This allows you to create a two-way communication channel between Rust and JS.
-
-        let mut eval_1 = eval(
-            r#"
-                let msg = await dioxus.recv();
-                console.log(msg);
-                const rrr = window.PlsRender;
-                console.log(rrr);
-                rrr();
-                return
-            "#,
-        );
-
-        // Send a message to the JS code.
-        eval_1.send("Hi from Rust!".into()).unwrap();
-
-        // Our line on the JS side will log the message and then return "hello world".
-        let res = eval_1.recv().await.unwrap();
-
-        // This will print "Hi from JS!" and "Hi from Rust!".
-        let _res_1 = eval_1.await;
-
-        res
+fn TwoWay() -> Element {
+    let mut count = use_signal(|| 0);
+    let eval = eval(
+        r#"
+        console.log("evall");
+        window.addEventListener("ping-me", (e) => {
+            console.log(e);
+            dioxus.send(e.detail);
+        });
+        "#,
+    );
+    spawn(async move {
+        dioxus_logger::tracing::info!("starting");
+        to_owned![eval];
+        while let Ok(msg) = eval.recv().await {
+            dioxus_logger::tracing::info!("msg: {}", msg);
+            count += 1;
+        }
     });
-
-    future.value().as_ref();
-    rsx!(div {})
+    rsx! {
+        div {
+            "loop"
+        }
+    }
 }
+
+// #[component]
+// fn React() -> Element {
+//     // Create a future that will resolve once the javascript has been successfully executed.
+//     let future = use_resource(move || async move {
+//         // The `eval` is available in the prelude - and simply takes a block of JS.
+//         // Dioxus' eval is interesting since it allows sending messages to and from the JS code using the `await dioxus.recv()`
+//         // builtin function. This allows you to create a two-way communication channel between Rust and JS.
+//
+//         let mut eval_1 = eval(
+//             r#"
+//                 let msg = await dioxus.recv();
+//                 console.log(msg);
+//                 const rrr = window.PlsRender;
+//                 console.log(rrr);
+//                 rrr();
+//                 return
+//             "#,
+//         );
+//
+//         // Send a message to the JS code.
+//         eval_1.send("Hi from Rust!".into()).unwrap();
+//
+//         // Our line on the JS side will log the message and then return "hello world".
+//         let res = eval_1.recv().await.unwrap();
+//
+//         // This will print "Hi from JS!" and "Hi from Rust!".
+//         let _res_1 = eval_1.await;
+//
+//         res
+//     });
+//
+//     future.value().as_ref();
+//     rsx!(div {})
+// }
